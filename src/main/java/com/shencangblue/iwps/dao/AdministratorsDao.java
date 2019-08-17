fb8f7e7174da3ad385fd9f6853cf14b611b064a9
shencang <2630610009@qq.com>
2019-08-17 23:22:14 +0800

    继续完善

--- src/main/java/com/shencangblue/iwps/dao/AdministratorsDao.java
+++ src/main/java/com/shencangblue/iwps/dao/AdministratorsDao.java
@@ -1,9 +1,15 @@
 package com.shencangblue.iwps.dao;
 
+import com.shencangblue.iwps.entity.Administrators;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface AdministratorsDao {
 
+    Administrators queryById(long id);
+    List<Administrators> queryAll(@Param("offset") int offset,@Param("limit") int limit);
+    int addAdministrators(Administrators administrators);
+    int udpateAdministrators(Administrators administrators);
+    int deleteAdministrators(long id);
 }
--- src/main/java/com/shencangblue/iwps/entity/Administrators.java
+++ src/main/java/com/shencangblue/iwps/entity/Administrators.java
@@ -1,4 +1,10 @@
 package com.shencangblue.iwps.entity;
 
 public class Administrators {
+
+    private long administratorId;
+    private String name;
+    private String avater;
+    private String detail;
+
 }
