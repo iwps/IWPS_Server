27b5e15c4cb20de927d3a599004342aff125ba98
shencang <2630610009@qq.com>
2019-09-20 04:35:12 +0800

    视图补充

--- src/main/java/com/shencangblue/iwps/controller/hardware/HistoryController.java
+++ src/main/java/com/shencangblue/iwps/controller/hardware/HistoryController.java
@@ -2,29 +2,27 @@ package com.shencangblue.iwps.controller.hardware;
 
 import com.shencangblue.iwps.dto.NatatoriumHistoryDto;
 import com.shencangblue.iwps.dto.ResponseObject;
+import com.shencangblue.iwps.dto.UserHistoryDto;
 import com.shencangblue.iwps.entity.NatatoriumHistory;
 import com.shencangblue.iwps.entity.UserHistory;
 import com.shencangblue.iwps.service.NatatoriumHistoryService;
 import com.shencangblue.iwps.service.UserHistoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/History")
+@RequestMapping("/history")
 public class HistoryController {
 
-    private final UserHistoryService historyService;
+    private final UserHistoryService userHistoryService;
     private final NatatoriumHistoryService natatoriumHistoryService;
 
     @Autowired
-    public HistoryController(UserHistoryService historyService, NatatoriumHistoryService natatoriumHistoryService) {
-        this.historyService = historyService;
+    public HistoryController(UserHistoryService userHistoryService, NatatoriumHistoryService natatoriumHistoryService) {
+        this.userHistoryService = userHistoryService;
         this.natatoriumHistoryService = natatoriumHistoryService;
     }
 
@@ -47,5 +45,39 @@ public class HistoryController {
         responseObject.setCount(natatoriumHistoryService.selectCount());
         return responseObject;
 
+    }
+
+    /**
+     * 添加nata数据
+     */
+    @ResponseBody
+    @RequestMapping(value = "/addNataHis", method = RequestMethod.POST)
+    public ResponseObject addNataHis(@RequestBody NatatoriumHistoryDto dto){
+        System.out.println("addNataHis");
+        ResponseObject<String> responseObject=new ResponseObject<>();
+        if(natatoriumHistoryService.add(dto)){
+            System.out.println("添加成功！");
+            responseObject.setMsg("OK");
+            responseObject.setCount(1);
+            responseObject.setCode(0);
+        }
+        return responseObject;
+    }
+
+    /**
+     * 添加user数据
+     */
+    @ResponseBody
+    @RequestMapping(value = "/addUserHis", method = RequestMethod.POST)
+    public ResponseObject addUserHis(@RequestBody UserHistoryDto dto){
+        System.out.println("addUserHis");
+        ResponseObject<String> responseObject=new ResponseObject<>();
+        if(userHistoryService.add(dto)){
+            System.out.println("添加成功！");
+            responseObject.setMsg("OK");
+            responseObject.setCount(1);
+            responseObject.setCode(0);
+        };
+        return responseObject;
     }
 }
